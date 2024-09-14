5515165bbf4ed65d19a4791cd205374e872ad72a
eluxbar <72967925+eluxbar@users.noreply.github.com>
2024-09-14 23:57:38 +0100

    Update fxmanifest.lua

--- chat/fxmanifest.lua
+++ chat/fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'bodacious' -- hiyayouknob
 game 'gta5' --ggggg
--- slothid made these -- (wow im 
+-- slothid made these -- (wow i
 ui_page 'html/index.html'
 im the goatd
 client_script 'cl_chat.lua'
