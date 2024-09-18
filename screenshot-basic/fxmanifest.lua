f215cef6719d5df83a79f3c15cb488bd29113057
eluxbar <72967925+eluxbar@users.noreply.github.com>
2024-09-18 15:35:14 +0100

    Update fxmanifest.lua

--- screenshot-basic/fxmanifest.lua
+++ screenshot-basic/fxmanifest.lua
@@ -19,6 +19,7 @@
 ------------fx_version 'bodacious'
 ------------fx_version 'bodacious'
 ------------fx_version 'bodacious'
+------------fx_version 'bodacious'
 game 'common'
 
 client_script 'dist/client.js'
