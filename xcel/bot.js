a1c5ef4c16921feacb0a1ef607db506abb3e44f2
eluxbar <72967925+eluxbar@users.noreply.github.com>
2024-09-25 23:54:57 +0100

    Update bot.js

--- xcel/bot.js
+++ xcel/bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js'); // www2232
+const Discord = require('discord.js'); // www22323
 const client = new Discord.Client();
 const path = require('path')
 const resourcePath = global.GetResourcePath ?
